c1d9a3d8a3869b0788d1a5c2701b842e0e54d882
Diego Madrid <diegomadridmu123@gmail.com>
2025-03-01 22:06:32 -0500

    Se Soluciona el error de swagger

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-import express from 'express';
-import userRouter from './routes/user.routes.js';
+import express from "express";
+import userRouter from "./routes/user.routes.js";
+import swaggerUI from "swagger-ui-express";
+import fs from "fs";
 
 const app = express();
-
-import swagerUI from 'swagger-ui-express';
-import swagerDocumentation from './swagger.json' assert { type: 'json' };
-
-app.use(express.json());
 const PORT = 3000;
-
-app.use('/doc', swagerUI.serve, swagerUI.setup(swagerDocumentation));
+const swaggerDocumentation = JSON.parse(fs.readFileSync("./swagger.json", "utf-8"));
+app.use(express.json());
+app.use("/doc", swaggerUI.serve, swaggerUI.setup(swaggerDocumentation));
 app.use("/api", userRouter);
 
 app.listen(PORT, () => {
-    console.log("Express.js está corriendo en el puerto " + PORT);
+  console.log("Express.js está corriendo en el puerto " + PORT);
 });

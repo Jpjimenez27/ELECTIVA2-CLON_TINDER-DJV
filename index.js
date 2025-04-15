d9cc131b1c51bcef3263a662341ea888245bb28c
Diego Madrid <analista8@mic.com.co>
2025-04-15 07:53:15 -0500

    SE IMPLEMENTA SOCKETS

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 import express from "express";
+import http from "http";
 import cors from "cors";
+import { Server } from "socket.io";
 import { authUserRouter } from "./src/application/routes/authUser.routes.js";
 import { hobbiesRouter } from "./src/application/routes/hobbies.routes.js";
 import swaggerUI from "swagger-ui-express";
 import fs from "fs";
 
 const app = express();
-const PORT = 3000;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
+});
 
+const PORT = 3000;
 const swaggerDocumentation = JSON.parse(fs.readFileSync("./swagger.json", "utf-8"));
 
+// Middlewares
 app.use(cors()); 
 app.use(express.json());
-
-
 app.use("/doc", swaggerUI.serve, swaggerUI.setup(swaggerDocumentation));
 app.use("/api/authusers", authUserRouter);
 app.use("/api/hobbies", hobbiesRouter);
 
+io.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado: " + socket.id);
 
-app.listen(PORT, () => {
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`Usuario unido a la sala ${room}`);
+  });
+
+  
+  socket.on("send_message", ({ room, body }) => {
+    console.log("Mensaje recibido:", body);
+    io.to(room).emit("receive_message", body);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado: " + socket.id);
+  });
+});
+
+server.listen(PORT, () => {
   console.log("Express.js está corriendo en el puerto " + PORT);
 });
--- src/application/controllers/hobbiesController.js
+++ src/application/controllers/hobbiesController.js
@@ -3,7 +3,7 @@ import pool from "./../../infrastructure/databases/mySqlRepository.js";
 export const getHobbies = async (req, resp) => {
     try {
         const [rows] = await pool.query("select * from hobbies");    
-        console.log(rows);
+
         resp.status(200).send(rows);
     
     } catch (error) {

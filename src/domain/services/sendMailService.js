80e58d6c8412439881e840973dfdb18f1485e28f
VICTOR VASQUEZ <v.avasquez433@gmail.com>
2025-05-01 17:08:57 -0500

    Refactorizacion ennvio de correo

--- src/domain/services/sendMailService.js
+++ src/domain/services/sendMailService.js
@@ -1,43 +1,52 @@
-import nodemailer from 'nodemailer';
-import fs from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import nodemailer from "nodemailer";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-export const sendMailActivateAccount = async (name, urlActive) => {
-    try {
-        // const htmlContent = fs.readFileSync(path.resolve(htmlTemplatePath), 'utf8');
+export const sendMailActivateAccount = async (name, urlActive, email) => {
+  try {
+    // const htmlContent = fs.readFileSync(path.resolve(htmlTemplatePath), 'utf8');
 
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = dirname(__filename);
-        const templatePath = path.join(__dirname, "..", "..", "application", "templates", "SendActivateAccountTemplate.html");
-        console.log(templatePath);
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const templatePath = path.join(
+      __dirname,
+      "..",
+      "..",
+      "application",
+      "templates",
+      "SendActivateAccountTemplate.html"
+    );
+    console.log(templatePath);
 
-        const htmlTemplate = (await fs.readFile(templatePath, 'utf-8')).replace("{Nombre}","Diego Madrid");
-        // Configurar el transporter con Outlook SMTP
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'flamematchapp07@gmail.com',
-                pass: 'giio gakz vjoo pioi',
-            },
-            tls: {
-                ciphers: 'SSLv3'
-            }
-        });
+    const htmlTemplate = (await fs.readFile(templatePath, "utf-8"))
+      .replace("{Nombre}", name)
+      .replace("{urlActive}", urlActive)
 
-        // Opciones del correo
-        const mailOptions = {
-            from: "flamematchapp07@gmail.com",
-            to: "diego.madrid@correo.tdea.edu.co",
-            subject: "Activa tu cuenta❤️‍🔥🔥",
-            html: htmlTemplate
-        };
+    // Configurar el transporter con Outlook SMTP
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "flamematchapp07@gmail.com",
+        pass: "giio gakz vjoo pioi",
+      },
+      tls: {
+        ciphers: "SSLv3",
+      },
+    });
 
-        // Enviar correo
-        const info = await transporter.sendMail(mailOptions);
+    // Opciones del correo
+    const mailOptions = {
+      from: "flamematchapp07@gmail.com",
+      to: email,
+      subject: "Activa tu cuenta",
+      html: htmlTemplate,
+    };
 
-    } catch (err) {
-        console.error('❌ Error al enviar el correo:', err);
-    }
+    // Enviar correo
+    const info = await transporter.sendMail(mailOptions);
+  } catch (err) {
+    console.error(" Error al enviar el correo:", err);
+  }
 };
